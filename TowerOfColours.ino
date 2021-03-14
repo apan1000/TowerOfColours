76a8081949df483af4a3b0ea9c41a7561d004bd0
Fredrik Berglund <fredrik.911225@gmail.com>
2021-03-14 21:01:22 +0100

    Improve performance of HTTP requests (#1)
    * Improve performance of HTTP requests
    
    * Shorten day get interval

--- TowerOfColours.ino
+++ TowerOfColours.ino
@@ -1,6 +1,5 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
-#include <ArduinoHttpClient.h>
 #include <ArduinoJson.h>
 
 #include "NeoPatterns.h"
@@ -15,22 +14,30 @@
 
 #define RAINBOW_SPEED 400
 
+#define RESPONSE_STATUS_CODE_ERROR_INDEX 0
+#define WIFI_ERROR_INDEX 2
+#define START_ERROR_INDEX (NUMPIXELS / 2)
+
 void onPatternComplete();
 NeoPatterns strip(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800, &onPatternComplete);
 
 ///////please enter your sensitive data in arduino_secrets.h
 char ssid[] = SECRET_SSID; // your network SSID (name)
 char pass[] = SECRET_PASS; // your network password
+char server[] = SERVER; // Server address
+
 int status = WL_IDLE_STATUS; // the Wifi radio's status
 
 unsigned long lastConnectionTime = 0; // last time you connected to the server, in milliseconds
-const unsigned long getIntervalNight = 1400;
-const unsigned long getIntervalDay = 20L * 60L * 1000L;
+const unsigned long getIntervalNight = 1000;
+const unsigned long getIntervalDay = 2L * 60L * 1000L;
 
-char server[] = SERVER; // Server address
+WiFiClient wifiClient;
 
-WiFiClient wifi;
-HttpClient client = HttpClient(wifi, server);
+int responseStatusCode = 200;
+
+String response;
+bool hasNewResponse = false;
 
 struct RGB {
   byte r;
@@ -38,9 +45,11 @@ struct RGB {
   byte b;
 };
 
+StaticJsonDocument<1024> doc;
 RGB towerColours[10];
 bool isDay = false;
 bool isBusy = false;
+int statusCode = 1;
 
 void setup() {
   strip.begin();
@@ -50,26 +59,35 @@ void setup() {
   Serial.begin(9600);
 
   if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
+    Serial.println(F("Communication with WiFi module failed!"));
+    showErrorColors(START_ERROR_INDEX);
     // don't continue
     while (true);
   }
 
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
-
   connectToWifi();
-
-  Serial.print("You're connected to the network");
-  printCurrentNet();
-  printWifiData();
 }
 
 void loop() {
   checkWiFi();
-  const unsigned long interval = millis() - lastConnectionTime;
+
+  while (wifiClient.available()) {
+    hasNewResponse = true;
+    char c = wifiClient.read();
+    response += c;
+  }
+
+  if (hasNewResponse) {
+    handleResponse();
+  }
+
+  const unsigned long now = millis();
+  unsigned long interval;
+  if (now >= lastConnectionTime) {
+    interval = now - lastConnectionTime;
+  } else {
+    interval = now;
+  }
 
   if (isDay) {
     strip.setBrightness(BRIGHTNESS_DAY);
@@ -78,26 +96,61 @@ void loop() {
       getColours();
     }
   } else {
+    setStripColors();
     if (interval > getIntervalNight) {
       strip.setBrightness(BRIGHTNESS_NIGHT);
       getColours();
-      setStripColors();
     }
+  }
+
+  if (responseStatusCode != 200) {
+    showErrorColors(RESPONSE_STATUS_CODE_ERROR_INDEX);
   }
 }
 
 void getColours() {
-  client.get("/cbn-live/getColours");
+  wifiClient.stop();
+  response = "";
 
-  if (client.responseStatusCode() == 200) {
-    String response = client.responseBody();
-    Serial.print("Response: ");
-    Serial.println(response);
+  if (wifiClient.connect(server, 80)) {
+    char hostHeader[40];
+    sprintf(hostHeader, "Host: %s", server);
 
-    deserializeJSON(response.c_str());
+    wifiClient.println("GET /cbn-live/getColours HTTP/1.1");
+    wifiClient.println(hostHeader);
+    wifiClient.println("Connection: close");
+    wifiClient.println();
   }
-
   lastConnectionTime = millis();
+}
+
+void handleResponse() {
+  responseStatusCode = getStatusCode(response);
+  if (responseStatusCode == 200) {
+    auto body = getResponseBody(response);
+    if (strlen(body) != 0) {
+      deserializeJSON(body);
+    }
+  }
+  hasNewResponse = false;
+}
+
+int getStatusCode(const String& response) {
+  int statusCode = 0;
+  if (response.length() > 12) {
+    statusCode = response.substring(9,12).toInt();
+  }
+  return statusCode;
+}
+
+const char* getResponseBody(const String& response) {
+  int index = response.indexOf("\r\n\r\n");
+  if (index != -1) {
+    index += 4;
+    auto body = response.substring(index);
+    return body.c_str();
+  }
+  return "";
 }
 
 void setStripColors() {
@@ -118,6 +171,17 @@ void setStripColors() {
     strip.setPixelColor(0, strip.Color(0, 50, 0));
   }
 
+  if (statusCode != 1) {
+    strip.setPixelColor(0, strip.Color(200, 160, 0));
+  }
+
+  strip.show();
+}
+
+void showErrorColors(int startIndex) {
+  for (int i = startIndex; i < strip.numPixels(); i += 2) {
+    strip.setPixelColor(i, strip.Color(255, 0, 0));
+  }
   strip.show();
 }
 
@@ -128,97 +192,85 @@ void onPatternComplete() {
 void checkWiFi() {
   status = WiFi.status();
   if (status == WL_CONNECTION_LOST || status == WL_DISCONNECTED) {
+    showErrorColors(WIFI_ERROR_INDEX);
     connectToWifi();
   }
 }
 
 void connectToWifi() {
   while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
     status = WiFi.begin(ssid, pass);
     delay(5000);
   }
 }
 
-void printCurrentNet() {
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.println(rssi);
-}
-
-void printWifiData() {
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-  Serial.println(ip);
-}
-
 void deserializeJSON(const char* json) {
-  const size_t capacity = 10*JSON_ARRAY_SIZE(3) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(10) + 90;
-  DynamicJsonDocument doc(capacity);
+  DeserializationError err = deserializeJson(doc, json);
 
-  deserializeJson(doc, json);
+  if (!err) {
+    statusCode = doc["status"]["code"]; // 1
+    const char* statusDescription = doc["status"]["description"]; // "ok"
+  } else {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(err.c_str());
+    statusCode = -1;
+  }
 
-  int status_code = doc["status"]["code"]; // 1
-  const char* status_description = doc["status"]["description"]; // "ok"
+  if (statusCode == 1) {
+    JsonObject colours = doc["colours"];
 
-  JsonObject colours = doc["colours"];
+    JsonArray colours_0 = colours["0"];
+    towerColours[0].r = colours_0[0];
+    towerColours[0].g = colours_0[1];
+    towerColours[0].b = colours_0[2];
 
-  JsonArray colours_0 = colours["0"];
-  towerColours[0].r = colours_0[0];
-  towerColours[0].g = colours_0[1];
-  towerColours[0].b = colours_0[2];
+    JsonArray colours_1 = colours["1"];
+    towerColours[1].r = colours_1[0];
+    towerColours[1].g = colours_1[1];
+    towerColours[1].b = colours_1[2];
 
-  JsonArray colours_1 = colours["1"];
-  towerColours[1].r = colours_1[0];
-  towerColours[1].g = colours_1[1];
-  towerColours[1].b = colours_1[2];
+    JsonArray colours_2 = colours["2"];
+    towerColours[2].r = colours_2[0];
+    towerColours[2].g = colours_2[1];
+    towerColours[2].b = colours_2[2];
 
-  JsonArray colours_2 = colours["2"];
-  towerColours[2].r = colours_2[0];
-  towerColours[2].g = colours_2[1];
-  towerColours[2].b = colours_2[2];
+    JsonArray colours_3 = colours["3"];
+    towerColours[3].r = colours_3[0];
+    towerColours[3].g = colours_3[1];
+    towerColours[3].b = colours_3[2];
 
-  JsonArray colours_3 = colours["3"];
-  towerColours[3].r = colours_3[0];
-  towerColours[3].g = colours_3[1];
-  towerColours[3].b = colours_3[2];
+    JsonArray colours_4 = colours["4"];
+    towerColours[4].r = colours_4[0];
+    towerColours[4].g = colours_4[1];
+    towerColours[4].b = colours_4[2];
 
-  JsonArray colours_4 = colours["4"];
-  towerColours[4].r = colours_4[0];
-  towerColours[4].g = colours_4[1];
-  towerColours[4].b = colours_4[2];
+    JsonArray colours_5 = colours["5"];
+    towerColours[5].r = colours_5[0];
+    towerColours[5].g = colours_5[1];
+    towerColours[5].b = colours_5[2];
 
-  JsonArray colours_5 = colours["5"];
-  towerColours[5].r = colours_5[0];
-  towerColours[5].g = colours_5[1];
-  towerColours[5].b = colours_5[2];
+    JsonArray colours_6 = colours["6"];
+    towerColours[6].r = colours_6[0];
+    towerColours[6].g = colours_6[1];
+    towerColours[6].b = colours_6[2];
 
-  JsonArray colours_6 = colours["6"];
-  towerColours[6].r = colours_6[0];
-  towerColours[6].g = colours_6[1];
-  towerColours[6].b = colours_6[2];
+    JsonArray colours_7 = colours["7"];
+    towerColours[7].r = colours_7[0];
+    towerColours[7].g = colours_7[1];
+    towerColours[7].b = colours_7[2];
 
-  JsonArray colours_7 = colours["7"];
-  towerColours[7].r = colours_7[0];
-  towerColours[7].g = colours_7[1];
-  towerColours[7].b = colours_7[2];
+    JsonArray colours_8 = colours["8"];
+    towerColours[8].r = colours_8[0];
+    towerColours[8].g = colours_8[1];
+    towerColours[8].b = colours_8[2];
 
-  JsonArray colours_8 = colours["8"];
-  towerColours[8].r = colours_8[0];
-  towerColours[8].g = colours_8[1];
-  towerColours[8].b = colours_8[2];
+    JsonArray colours_9 = colours["9"];
+    towerColours[9].r = colours_9[0];
+    towerColours[9].g = colours_9[1];
+    towerColours[9].b = colours_9[2];
 
-  JsonArray colours_9 = colours["9"];
-  towerColours[9].r = colours_9[0];
-  towerColours[9].g = colours_9[1];
-  towerColours[9].b = colours_9[2];
-
-  isDay = doc["isDay"];
-  isBusy = doc["isBusy"];
-  bool inCall = doc["inCall"];
+    isDay = doc["isDay"];
+    isBusy = doc["isBusy"];
+    bool inCall = doc["inCall"];
+  }
 }
